e26178899c6526d9f75e3b6b8a3baedc9415cd3b
bun-boy <byash.iitk@gmail.com>
2022-05-22 02:54:16 +0530

    js done!!

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
-console.log("hi");
-
 const main = document.querySelector(".maincontent");
 
+const updateRow = function (event, item, newValue) {
+  console.log(newValue);
+  let title = document.querySelector(".item__title" + item.id);
+  item.title = newValue;
+  title.innerHTML = `${item.title}`;
+};
+
+const loadItem = function (event, item) {
+  console.log(item);
+  const imageArticle = document.createElement("article");
+  imageArticle.classList.add("image");
+  imageArticle.setAttribute("id", item.id);
+  imageArticle.innerHTML = `
+    <figure class="image__src">
+      <img src=${item.previewImage} loading="lazy" />
+    </figure>
+    <form>
+      <label for="image__title"></label>
+      <input type="text" value=${item.title}>
+    </form>
+  `;
+  let form = imageArticle.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let newValue = form.querySelector("input").value;
+    updateRow(event, item, newValue);
+  });
+  main.append(imageArticle);
+};
+
+const formed = function (item, id) {
+  console.log(item);
+  let row = document.createElement("row");
+  row.classList.add("tab");
+  row.setAttribute("id", id);
+  row.innerHTML = `
+    <h1 class="item__title${item.id}">${item.title}</h1>
+  `;
+  row.addEventListener("click", (event) => {
+    loadItem(event, item);
+  });
+  return row;
+};
+
 const printHTML = function (items) {
-  let count = 0;
+  let id = 1;
   items.forEach((item) => {
-    console.log(item);
-    let imageTab = document.createElement("tab");
-    imageTab.setAttribute("id", ++count);
-    imageTab.classList.add("item");
-    imageTab.innerHTML = `
-        <figure class="item__image">
-            <img src=${item.previewImage} alt="" loading="lazy" />
-        </figure>
-        <h1 class="item__title">${item.title}</h1>
-    `;
-    main.append(imageTab);
+    item.id = id;
+    main.append(formed(item, id++));
   });
 };
 
 fetch("./items.json")
   .then((response) => response.text())
   .then((json) => {
-    console.log(json);
     let items = JSON.parse(json);
-    // console.log(data);
     printHTML(items);
   });
